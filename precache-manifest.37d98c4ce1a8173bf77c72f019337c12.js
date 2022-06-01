7fbc9d4e4b5a0a91be2c2b73cffc3757b7e09d10
evgeniy.borysov <evgeniy.borysov@gmail.com>
2022-06-01 14:17:54 +0300

    Updates

---
+++ precache-manifest.37d98c4ce1a8173bf77c72f019337c12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad42a93a03efe4fc0d608d7c342635e6",
+    "url": "/TODO_react/index.html"
+  },
+  {
+    "revision": "b300f8da67a273d6c8da",
+    "url": "/TODO_react/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "76f8d08f09736743dc24",
+    "url": "/TODO_react/static/js/2.450f50a3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TODO_react/static/js/2.450f50a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b300f8da67a273d6c8da",
+    "url": "/TODO_react/static/js/main.47726624.chunk.js"
+  },
+  {
+    "revision": "f1ba30b4aebe5f31d249",
+    "url": "/TODO_react/static/js/runtime-main.6c844d67.js"
+  }
+]);
